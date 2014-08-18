32932c51382ac64679f102fe0aeadcddf0fc8065
Teemu Sarapisto <tsarpf@gmail.com>
2014-08-18 23:45:16 +0300

    Added project barebones

---
+++ Puzzle/Assets/Scripts/Initializer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Initializer : MonoBehaviour {
+
+    Object line;
+    Triangle[][] pyramid;
+	// Use this for initialization
+	void Start () {
+        Triangle.triangles[0] = Resources.Load("FirstTrianglePrefab");
+        Triangle.triangles[1] = Resources.Load("SecondTrianglePrefab");
+        Triangle.triangles[2] = Resources.Load("ThirdTrianglePrefab");
+
+
+	}
+
+    void constructPyramid(int floorCount)
+    {
+        int bottomLevelTriangleCount = floorCount * 2 - 1;
+        
+        pyramid = new Triangle[bottomLevelTriangleCount][];
+
+        //Bottom level is index 0
+        for(int y = 0; y < floorCount; y++)
+        {
+           for(int x = 0; x < y * 2 - 1; x++)
+           {
+
+           }
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
